6944602aca2ae5947f2effe42e6a78687ae5d748
Christopher L <cmlewis1272@yahoo.com>
2025-04-14 14:13:24 -0500

    Update vmTranslator.js

--- nand2tetris-web/js/vmTranslator/vmTranslator.js
+++ nand2tetris-web/js/vmTranslator/vmTranslator.js
@@ -1,13 +1,20 @@
 // js/vmTranslator/vmTranslator.js
 
-let labelCounter = 0;
+const SEGMENT_BASES = {
+  local: 'LCL',
+  argument: 'ARG',
+  this: 'THIS',
+  that: 'THAT',
+};
 
 export function translateVM(vmCode) {
   const lines = vmCode.split(/\r?\n/).map(line => line.trim()).filter(line => line && !line.startsWith('//'));
   const output = [];
 
   for (const line of lines) {
-    const [command, arg1, arg2] = line.split(' ');
+    const tokens = line.split(' ');
+    const [command, arg1, arg2] = tokens;
+    output.push(`// ${line}`);
     output.push(...translateCommand(command, arg1, arg2));
   }
 
@@ -19,26 +26,91 @@ function translateCommand(cmd, arg1, arg2) {
     case 'add': return ['@SP', 'AM=M-1', 'D=M', 'A=A-1', 'M=M+D'];
     case 'sub': return ['@SP', 'AM=M-1', 'D=M', 'A=A-1', 'M=M-D'];
     case 'neg': return ['@SP', 'A=M-1', 'M=-M'];
-    case 'eq':  return compareTemplate('JEQ');
-    case 'gt':  return compareTemplate('JGT');
-    case 'lt':  return compareTemplate('JLT');
+    case 'eq': return compareTemplate('JEQ');
+    case 'gt': return compareTemplate('JGT');
+    case 'lt': return compareTemplate('JLT');
     case 'and': return ['@SP', 'AM=M-1', 'D=M', 'A=A-1', 'M=M&D'];
-    case 'or':  return ['@SP', 'AM=M-1', 'D=M', 'A=A-1', 'M=M|D'];
+    case 'or': return ['@SP', 'AM=M-1', 'D=M', 'A=A-1', 'M=M|D'];
     case 'not': return ['@SP', 'A=M-1', 'M=!M'];
+
     case 'push':
-      if (arg1 === 'constant') {
-        return [`@${arg2}`, 'D=A', '@SP', 'A=M', 'M=D', '@SP', 'M=M+1'];
-      }
-      // Other memory segments will go here later
-      break;
+      return pushCommand(arg1, arg2);
     case 'pop':
-      // We’ll implement this soon
-      break;
+      return popCommand(arg1, arg2);
+
     default:
       return [`// Unknown command: ${cmd}`];
   }
+}
 
-  return [`// Unsupported: ${cmd} ${arg1} ${arg2}`];
+function pushCommand(segment, index) {
+  switch (segment) {
+    case 'constant':
+      return [`@${index}`, 'D=A', '@SP', 'A=M', 'M=D', '@SP', 'M=M+1'];
+    case 'local':
+    case 'argument':
+    case 'this':
+    case 'that':
+      return [
+        `@${index}`, 'D=A',
+        `@${SEGMENT_BASES[segment]}`, 'A=M+D', 'D=M',
+        '@SP', 'A=M', 'M=D',
+        '@SP', 'M=M+1'
+      ];
+    case 'temp':
+      return [
+        `@${5 + Number(index)}`, 'D=M',
+        '@SP', 'A=M', 'M=D',
+        '@SP', 'M=M+1'
+      ];
+    case 'pointer':
+      return [
+        `@${3 + Number(index)}`, 'D=M',
+        '@SP', 'A=M', 'M=D',
+        '@SP', 'M=M+1'
+      ];
+    case 'static':
+      return [
+        `@Static.${index}`, 'D=M',
+        '@SP', 'A=M', 'M=D',
+        '@SP', 'M=M+1'
+      ];
+    default:
+      return [`// Unsupported push segment: ${segment}`];
+  }
+}
+
+function popCommand(segment, index) {
+  switch (segment) {
+    case 'local':
+    case 'argument':
+    case 'this':
+    case 'that':
+      return [
+        `@${index}`, 'D=A',
+        `@${SEGMENT_BASES[segment]}`, 'D=M+D',
+        '@R13', 'M=D',
+        '@SP', 'AM=M-1', 'D=M',
+        '@R13', 'A=M', 'M=D'
+      ];
+    case 'temp':
+      return [
+        '@SP', 'AM=M-1', 'D=M',
+        `@${5 + Number(index)}`, 'M=D'
+      ];
+    case 'pointer':
+      return [
+        '@SP', 'AM=M-1', 'D=M',
+        `@${3 + Number(index)}`, 'M=D'
+      ];
+    case 'static':
+      return [
+        '@SP', 'AM=M-1', 'D=M',
+        `@Static.${index}`, 'M=D'
+      ];
+    default:
+      return [`// Unsupported pop segment: ${segment}`];
+  }
 }
 
 function compareTemplate(jumpType) {
@@ -55,5 +127,3 @@ function compareTemplate(jumpType) {
     `(${labelEnd})`
   ];
 }
-
-  
